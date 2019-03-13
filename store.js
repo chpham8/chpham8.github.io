c8e3405c40097fee352516c9c3131ce81c15f72f
Christopher Pham <chpham8@gmail.com>
2019-03-13 14:07:09 -0600

    added filter functionality

--- store.js
+++ store.js
@@ -12,6 +12,10 @@ function itemCreator(id, name, price, type, image, description){
 let content = document.getElementById('content');
 let cartBar = document.getElementById('cartBar');
 let checkout = document.getElementById('checkout');
+let allFilter = document.getElementById('all');
+let consolesFilter = document.getElementById('consoles');
+let accessoriesFilter = document.getElementById('accessories');
+let gameFilter = document.getElementById('games');
 
 let switchConsole = new itemCreator("n1", "Nintendo Switch", 299, "console", "images/switch.jpg", "Introducing Nintendo Switch, the new home video game system from Nintendo. In addition to providing single and multiplayer thrills at home, the Nintendo Switch system can be taken on the go so players can enjoy a full home console experience anytime, anywhere. The mobility of a handheld is now added to the power of a home gaming system, with unprecedented new play styles brought to life by the two new Joy-Con controllers.");
 let dsConsole = new itemCreator("n2", "Nintendo 3DS", 100, "console", "images/3ds.jpg", "Experience incredible gameplay featuring real 3D graphics, with no need for special glasses. Nintendo 3DS is a breakthrough in portable entertainment, a truly cutting-edge piece of hardware. It has to be seen to be believed.");
@@ -27,54 +31,10 @@ let qtyNum = document.getElementsByClassName('quantity');
 
 let cartItems =[];
 let cartQty = [];
+let filter = [];
 let cartTotal = 0;
 
-for (let i = 0; i<items.length; i++) {
-    let itemContainer = document.createElement("div");
-    itemContainer.className = 'contentItems';
-    itemContainer.id = items[i].id;
-    let img = document.createElement('img');
-    img.src = items[i].image;
-    itemContainer.appendChild(img);
-    let list = document.createElement('ul');
-    let item = document.createElement('li');
-    let name = document.createTextNode(items[i].name);
-    item.appendChild(name);
-    list.appendChild(item);
-    item = document.createElement('li');
-    let price = document.createTextNode("$" + items[i].price);
-    item.appendChild(price);
-    list.appendChild(item);
-
-    item = document.createElement('li');
-    let form = document.createElement('form');
-    let select = document.createElement("select");
-    select.className = "quantity";
-    for (let j = 1; j < 10; j++) {
-        let option = document.createElement("option");
-        option.value = j;
-        let optionText = document.createTextNode(j);
-        option.appendChild(optionText);
-        select.appendChild(option);
-    }
-    form.appendChild(select);
-    let button = document.createElement('button');
-    button.value=items[i].id;
-    button.type='button';
-    button.className="addToCart";
-    let buttonText =document.createTextNode("add to cart");
-    button.appendChild(buttonText);
-    form.appendChild(button);
-    item.appendChild(form);
-    list.appendChild(item);
-
-    item = document.createElement('li');
-    let description = document.createTextNode(items[i].description);
-    item.appendChild(description);
-    list.appendChild(item);
-    itemContainer.appendChild(list);
-    content.appendChild(itemContainer);
-}
+displayContent(items);
 
 for(let j = 0; j < items.length; j++) {
     cartButton[j].addEventListener('click', (event) => {
@@ -116,3 +76,94 @@ checkout.addEventListener('click', (event) => {
     checkoutContainer.appendChild(list);
     cartBar.appendChild(checkoutContainer);
 });
+
+allFilter.addEventListener('click', (event) => {
+   removeContent(content);
+   displayContent(items);
+});
+
+consolesFilter.addEventListener('click', (event) => {
+    removeContent(content);
+    filter = [];
+    filterContent('console', items);
+    displayContent(filter);
+});
+
+accessoriesFilter.addEventListener('click', (event) => {
+   removeContent(content);
+   filter = [];
+   filterContent('accessory', items);
+   displayContent(filter);
+});
+
+gameFilter.addEventListener('click', (event) => {
+   removeContent(content);
+   filter = [];
+   filterContent('game', items);
+   displayContent(filter);
+});
+
+function displayContent(array) {
+    for (let n = 0; n<array.length; n++) {
+        let itemContainer = document.createElement("div");
+        itemContainer.className = 'contentItems';
+        itemContainer.id = array[n].id;
+        let img = document.createElement('img');
+        img.src = array[n].image;
+        img.id = 'img' + array[n].id;
+        itemContainer.appendChild(img);
+        let list = document.createElement('ul');
+        let item = document.createElement('li');
+        let name = document.createTextNode(array[n].name);
+        item.appendChild(name);
+        list.appendChild(item);
+        item = document.createElement('li');
+        let price = document.createTextNode("$" + array[n].price);
+        item.appendChild(price);
+        list.appendChild(item);
+
+        item = document.createElement('li');
+        let form = document.createElement('form');
+        let select = document.createElement("select");
+        select.className = "quantity";
+        for (let j = 1; j < 10; j++) {
+            let option = document.createElement("option");
+            option.value = j;
+            let optionText = document.createTextNode(j);
+            option.appendChild(optionText);
+            select.appendChild(option);
+        }
+        form.appendChild(select);
+        let button = document.createElement('button');
+        button.value=array[n].id;
+        button.type='button';
+        button.className="addToCart";
+        let buttonText =document.createTextNode("add to cart");
+        button.appendChild(buttonText);
+        form.appendChild(button);
+        item.appendChild(form);
+        list.appendChild(item);
+
+        item = document.createElement('li');
+        let description = document.createTextNode(array[n].description);
+        item.appendChild(description);
+        list.appendChild(item);
+        itemContainer.appendChild(list);
+        content.appendChild(itemContainer);
+    }
+}
+
+function removeContent(container) {
+    while (container.hasChildNodes()){
+        container.removeChild(container.firstChild);
+    }
+}
+
+function filterContent(type, array){
+    for (let o=0; o<array.length; o++){
+        if (array[o].type === type){
+            filter.push(array[o]);
+        }
+    }
+    console.log(filter);
+}
