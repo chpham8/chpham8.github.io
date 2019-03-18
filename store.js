4c446a46873d7d371b6a0dd86525cafb7f319599
Christopher Pham <chpham8@gmail.com>
2019-03-17 22:25:26 -0600

    renamed html

--- store.js
+++ store.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// define the constructor to create the objects
 function itemCreator(id, name, price, type, image, description){
     this.id = id;
     this.name = name;
@@ -9,6 +10,18 @@ function itemCreator(id, name, price, type, image, description){
     this.description = description;
 }
 
+// call the constructor function to generate the objects and store it into a variable.
+let switchConsole = new itemCreator("n1", "Nintendo Switch", 299, "console", "images/switch.jpg", "Introducing Nintendo Switch, the new home video game system from Nintendo. In addition to providing single and multiplayer thrills at home, the Nintendo Switch system can be taken on the go so players can enjoy a full home console experience anytime, anywhere. The mobility of a handheld is now added to the power of a home gaming system, with unprecedented new play styles brought to life by the two new Joy-Con controllers.");
+let dsConsole = new itemCreator("n2", "Nintendo 3DS", 100, "console", "images/3ds.jpg", "Experience incredible gameplay featuring real 3D graphics, with no need for special glasses. Nintendo 3DS is a breakthrough in portable entertainment, a truly cutting-edge piece of hardware. It has to be seen to be believed.");
+let joyCon = new itemCreator("n3", "Nintendo Switch Joy-Con", 79, "accessory", "images/joycon.jpg", "Introducing Joy-Con, controllers that make new kinds of gaming possible, for use with Nintendo Switch.The versatile Joy-Con offer multiple surprising new ways for players to have fun. Two Joy-Con can be used independently in each hand, or together as one game controller when attached to the Joy-Con grip. They can also attach to the main console for use in handheld mode, or be shared with friends to enjoy two-player action in supported games.");
+let proCon = new itemCreator("n4", "Nintendo Switch Pro Controller", 60, "accessory", "images/procon.jpg", "Take your game sessions up a notch with the Pro Controller. Includes motion controls, HD rumble, built-in amiibo functionality, and more.");
+let smashBro = new itemCreator("n5", "Super Smash Bros. Ultimate", 60, "game", "images/smashbros.jpg", "Gaming icons clash in the ultimate brawl you can play anytime, anywhere! Smash rivals off the stage as new characters Simon Belmont and King K. Rool join Inkling, Ridley, and every fighter in Super Smash Bros. history. Enjoy enhanced speed and combat at new stages based on the Castlevania series, Super Mario Odyssey, and more!");
+let marioKart = new itemCreator("n6", "Mario Kart 9 Deluxe", 60, "game", "images/mariokart.jpg", "Hit the road with the definitive version of Mario Kart 9 and play anytime, any-where! Race your friends or battle them in a revised battle mode on new and returning battle courses. Play locally in up to 4-player multiplayer in 1080p while playing in TV Mode.")
+
+// define the item array with all of the object variables
+let items = [switchConsole, dsConsole, joyCon, proCon, smashBro, marioKart];
+
+// defining html elements to use later
 let content = document.getElementById('content');
 let cartBar = document.getElementById('cart');
 let checkout = document.getElementById('checkoutBox');
@@ -18,28 +31,21 @@ let accessoriesFilter = document.getElementById('accessories');
 let gameFilter = document.getElementById('games');
 let total = document.getElementById('total');
 let emptyMessage = document.getElementById('emptyCart');
-
-let switchConsole = new itemCreator("n1", "Nintendo Switch", 299, "console", "images/switch.jpg", "Introducing Nintendo Switch, the new home video game system from Nintendo. In addition to providing single and multiplayer thrills at home, the Nintendo Switch system can be taken on the go so players can enjoy a full home console experience anytime, anywhere. The mobility of a handheld is now added to the power of a home gaming system, with unprecedented new play styles brought to life by the two new Joy-Con controllers.");
-let dsConsole = new itemCreator("n2", "Nintendo 3DS", 100, "console", "images/3ds.jpg", "Experience incredible gameplay featuring real 3D graphics, with no need for special glasses. Nintendo 3DS is a breakthrough in portable entertainment, a truly cutting-edge piece of hardware. It has to be seen to be believed.");
-let joyCon = new itemCreator("n3", "Nintendo Switch Joy-Con", 79, "accessory", "images/joycon.jpg", "Introducing Joy-Con, controllers that make new kinds of gaming possible, for use with Nintendo Switch.The versatile Joy-Con offer multiple surprising new ways for players to have fun. Two Joy-Con can be used independently in each hand, or together as one game controller when attached to the Joy-Con grip. They can also attach to the main console for use in handheld mode, or be shared with friends to enjoy two-player action in supported games.");
-let proCon = new itemCreator("n4", "Nintendo Switch Pro Controller", 60, "accessory", "images/procon.jpg", "Take your game sessions up a notch with the Pro Controller. Includes motion controls, HD rumble, built-in amiibo functionality, and more.");
-let smashBro = new itemCreator("n5", "Super Smash Bros. Ultimate", 60, "game", "images/smashbros.jpg", "Gaming icons clash in the ultimate brawl you can play anytime, anywhere! Smash rivals off the stage as new characters Simon Belmont and King K. Rool join Inkling, Ridley, and every fighter in Super Smash Bros. history. Enjoy enhanced speed and combat at new stages based on the Castlevania series, Super Mario Odyssey, and more!");
-let marioKart = new itemCreator("n6", "Mario Kart 9 Deluxe", 60, "game", "images/mariokart.jpg", "Hit the road with the definitive version of Mario Kart 9 and play anytime, any-where! Race your friends or battle them in a revised battle mode on new and returning battle courses. Play locally in up to 4-player multiplayer in 1080p while playing in TV Mode.")
-
-let items = [switchConsole, dsConsole, joyCon, proCon, smashBro, marioKart];
-
 let cartButton = document.getElementsByClassName("addToCart");
 let qtyNum = document.getElementsByClassName('quantity');
 
+// define and initialize global variables to be used by all of the functions
 let cartItems =[];
 let cartQty = [];
 let filter = [];
 let cartTotal = 0;
 
-displayContent(items);
-addCartButton(items);
+// on page load function calls to set up the page
+displayContent(items); // function to display the array in the content div
+addCartButton(items); // function to link cart buttons to each product's div
 emptyCart();
 
+// this function creates an event listener for each product's button
 function addCartButton(array) {
     for (let i = 0; i < array.length; i++) {
         cartButton[i].addEventListener('click', (event) => {
